0da67e7de424c9ba74983295bf94c13309b05e02
smithg6 <smithg6@oregonstate.edu>
2016-10-28 19:50:54 -0700

    Added base parser implementation, can read, split, and reference command, then pass each element to room action checker.

--- parser.cpp
+++ parser.cpp
@@ -3,12 +3,50 @@
 //
 
 #include "parser.h"
+using namespace std;
+
+void parser::parse(parser *parser1, string commandIn) {
+
+    parser1->cmd = parser1->stringToLower(commandIn);
+
+    parser1->cmdVector = parser1->splitCommand(parser1);
+
+//    cout << "parser1.cmd value: " << parser1->cmd << endl;
+//    cout << "Below is each element of the command vector:" << endl;
+//    for(unsigned i; i < parser1->cmdVector.size(); i++){
+//        cout << "parser1.cmdVector value[" << i << "]:" << parser1->cmdVector[i] << endl;
+//    }
 
 
-void parser::setCommandIn(parser parser1, std::string commandIn) {
-    parser1.cmd = commandIn;
-    std::cout << "Command recorded: " << parser1.cmd << std::endl;
 }
 
 parser::parser(){};
+
+vector<string> parser::splitCommand(parser *parser1) {
+
+    stringstream ss(parser1->cmd);
+    istream_iterator<std::string> begin(ss);
+    istream_iterator<std::string> end;
+    vector<string> words(begin, end);
+    //std::copy(words.begin(), words.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
+
+    return words;
+
+}
+
+
+string parser::stringToLower(string input) {
+
+    for(unsigned i = 0; i < input.length(); i++) {
+        if (isupper(input[i])) {
+            input[i] = tolower(input.at(i));
+        }
+    }
+    //cout << "From inside stringToLower " << input << endl;
+    return input;
+}
+
+parser::~parser() {
+    cout << "Parser deconstruction successful." << endl;
+};
 
--- parser.h
+++ parser.h
@@ -5,24 +5,31 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-#include <../../boost_1_62_0/boost/tokenizer.hpp>
+
+//#include <../../boost_1_62_0/boost/tokenizer.hpp>
 #include <string>
+#include <sstream>
 #include <stdlib.h>
 #include <iostream>
 #include <vector>
+#include <iterator>
+using namespace std;
 
 class parser {
 
 public:
 
-    std::string cmd;
-    std::string verb;
-    std::string subject;
-    std::string object;
+    string cmd;
+    string verb;
+    string subject;
+    string object;
+    vector<string> cmdVector;
 
     parser();
-    void setCommandIn(parser parser1, std::string commandIn);
-    std::string splitCommand(void);
+    ~parser();
+    void parse(parser *parser1, std::string commandIn);
+    vector<string> splitCommand(parser *parser1);
+    string stringToLower(string input);
     void getVerb();
     void getSubject();
     void getObject();
