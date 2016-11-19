f1818eb72d338e99f724a48d10fc8582a3049c70
Robert Stine <robwstine@outlook.com>
2016-11-19 01:20:14 -0500

    Green Room 4 added, stubbed room class.

---
+++ Rooms/GreenRoomFour.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rob on 11/19/16.
+//
+
+#include "GreenRoomFour.h"
---
+++ Rooms/GreenRoomFour.h
@@ -0,0 +1,21 @@
+//
+// Created by rob on 11/19/16.
+//
+
+#ifndef ITEM_TEST_GREENROOMFOUR_H
+#define ITEM_TEST_GREENROOMFOUR_H
+
+
+#include "Room.h"
+
+class GreenRoomFour : public Room{
+private:
+    std::string testing();
+public:
+    GreenRoomFour(std::string, ItemTable *iList, bool f);
+    std::string getDescription();
+};
+
+
+
+#endif //ITEM_TEST_GREENROOMFOUR_H
